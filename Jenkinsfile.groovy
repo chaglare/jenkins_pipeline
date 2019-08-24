3f79b7e18c5a686613235a2f1ac0e9fc0f9de8af
Caglar Cavdar <chaglare@gmail.com>
2019-08-24 11:24:21 -0500

    added instrances

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 node {
     stage("Say Hello"){
+        properties([parameters([string(defaultValue: '3.18.214.214', description: '''Dev:3.18.214.214 QA:3.19.61.100 Prod:18.219.49.241''', name: 'Remote_instance', trim: true)])])
         git 'https://github.com/chaglare/jenkins.git'
     }
 }
