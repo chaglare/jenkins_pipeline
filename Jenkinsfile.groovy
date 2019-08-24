145947319df872b12cf7a814e520e0e41744f7e2
Caglar Cavdar <chaglare@gmail.com>
2019-08-24 11:28:24 -0500

    install http

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,9 @@
 node {
-    stage("Say Hello"){
+    stage("Pull Repo"){
         properties([parameters([string(defaultValue: '3.18.214.214', description: '''Dev:3.18.214.214 QA:3.19.61.100 Prod:18.219.49.241''', name: 'Remote_instance', trim: true)])])
         git 'https://github.com/chaglare/jenkins.git'
+    }
+    stage("Install Apache"){
+        sh "ssh   ec2-user@${Remote_instance}   sudo yum install httpd -y "
     }
 }
