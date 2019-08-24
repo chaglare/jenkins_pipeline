531d1a8aeeb21c5dd37376201e7c57502a8a702e
Caglar Cavdar <chaglare@gmail.com>
2019-08-24 11:16:36 -0500

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 node {
     stage("Say Hello"){
-        git "https://github.com/chaglare/jenkins.git"
-        sh "echo Hello ${NAME}"
+        git 'https://github.com/chaglare/jenkins.git'
     }
 }
